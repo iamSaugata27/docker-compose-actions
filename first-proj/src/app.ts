1b6074296cf7657fdff6a0137f5a25113d0f7074
iamSaugata27 <84305894+iamSaugata27@users.noreply.github.com>
2021-11-08 10:50:55 +0530

    docker push

---
+++ first-proj/src/app.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv'
+config()
+
+console.log('dot-env printing for project-1');
+console.log(process.env.PROJ1);
---
+++ second-proj/src/app.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv'
+config()
+
+console.log('dot-env printing for project-2');
+console.log(process.env.PROJ2);
